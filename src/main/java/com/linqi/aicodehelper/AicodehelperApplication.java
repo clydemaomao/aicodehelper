258b04130c01dffe48ea8533657f8eefb990b109
clydejiang1 <maomaoclyde@gmail.com>
2025-12-17 17:17:22 -0400

    first one

---
+++ src/main/java/com/linqi/aicodehelper/AicodehelperApplication.java
@@ -0,0 +1,13 @@
+package com.linqi.aicodehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicodehelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicodehelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linqi/aicodehelper/AicodehelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linqi.aicodehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicodehelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
